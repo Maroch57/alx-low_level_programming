e42b775e18d71bf49b1409b98b508c3dcd0543e2
jahath <jahathblessed@gmail.com>
2023-11-17 04:33:00 +0300

    singly linked list final

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,7 @@
  * l_word - function executed before main function
  * Return: void data type
  */
+
 void __attribute__((constructor)) l_word()
 {
 	printf("You're beat! and yet, you must allow,");
