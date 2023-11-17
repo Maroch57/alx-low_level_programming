142002ada70171883f7e886ffc063186a251df68
jahath <jahathblessed@gmail.com>
2023-11-17 04:41:03 +0300

    singly linked list final implementation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,9 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * l_word - function executed before main function
  * Return: void data type
  */
-
 void __attribute__((constructor)) l_word()
 {
 	printf("You're beat! and yet, you must allow,");
