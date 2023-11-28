6113d42c54401c00a63b7419b6c1dd61867497c9
Maroch57 <jahathblessed@gmail.com>
2023-11-28 04:28:24 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 /**
  * _error - reads a file.
@@ -27,36 +21,36 @@ void _error(int y, char *filename)
 	}
 }
 /**
- * cpy - Copies the content of a file to another file.
- * @init_file: Name of the source file.
- * @final_file: Name of the destination file.
+ * cp - Copies the content of a file to another file.
+ * @file_from: Name of the source file.
+ * @file_to: Name of the destination file.
  * Return: 1 if successful, -1 if it fails.
  */
-void cpy(char *init_file, char *final_file)
+void cp(char *file_from, char *file_to)
 {
 	int rd_read, rds_read, rd_write, rds_write;
 	char *bffer[1024];
 
 	/* READ */
-	rd_read = open(init_file, O_RDONLY);
+	rd_read = open(file_from, O_RDONLY);
 	if (rd_read < 0)
-		_error(98, init_file);
+		_error(98, file_from);
 	/* WRITE */
-	rd_write = open(final_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	rd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (rd_write < 0)
 	{
 		close(rd_read);
-		_error(99, final_file);
+		_error(99, file_to);
 	}
 	do {
 		/* READ */
 		rds_read = read(rd_read, bffer, 1024);
 		if (rds_read < 0)
-			_error(98, init_file);
+			_error(98, file_to);
 		/* WRITE */
 		rds_write = write(rd_write, bffer, rds_read);
 		if (rds_write < rds_read)
-			_error(99, final_file);
+			_error(99, file_from);
 	}	while (rds_write == 1024);
 	if (close(rd_read) < 0)
 	{
@@ -80,9 +74,9 @@ int main(int jk, char *fg[])
 {
 	if (jk != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cpy file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	cpy(fg[1], fg[2]);
+	cp(fg[1], fg[2]);
 	return (0);
 }
